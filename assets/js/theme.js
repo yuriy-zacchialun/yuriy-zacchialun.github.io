0f4aae631c861c85da39546f186e2201dd215322
Yuriy Zacchia Lun <yuriy@zacchialun.com>
2023-06-04 19:52:02 +0200

    Major update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -11,14 +11,26 @@ let toggleTheme = (theme) => {
 
 let setTheme = (theme) =>  {
   transTheme();
+  setHighlight(theme);
+  setGiscusTheme(theme);
+
   if (theme) {
     document.documentElement.setAttribute("data-theme", theme);
-  }
-  else {
+
+    // Add class to tables.
+    let tables = document.getElementsByTagName('table');
+    for(let i = 0; i < tables.length; i++) {
+      if (theme == "dark") {
+        tables[i].classList.add('table-dark');
+      } else {
+        tables[i].classList.remove('table-dark');
+      }
+    }
+  } else {
     document.documentElement.removeAttribute("data-theme");
   }
   localStorage.setItem("theme", theme);
-  
+
   // Updates the background of medium-zoom overlay.
   if (typeof medium_zoom !== 'undefined') {
     medium_zoom.update({
@@ -27,6 +39,34 @@ let setTheme = (theme) =>  {
     })
   }
 };
+
+
+let setHighlight = (theme) => {
+  if (theme == "dark") {
+    document.getElementById("highlight_theme_light").media = "none";
+    document.getElementById("highlight_theme_dark").media = "";
+  } else {
+    document.getElementById("highlight_theme_dark").media = "none";
+    document.getElementById("highlight_theme_light").media = "";
+  }
+}
+
+
+let setGiscusTheme = (theme) => {
+
+  function sendMessage(message) {
+    const iframe = document.querySelector('iframe.giscus-frame');
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+  }
+
+  sendMessage({
+    setConfig: {
+      theme: theme
+    }
+  });
+
+}
 
 
 let transTheme = () => {
@@ -38,12 +78,13 @@ let transTheme = () => {
 
 
 let initTheme = (theme) => {
-  if (theme == null) {
+  if (theme == null || theme == 'null') {
     const userPref = window.matchMedia;
     if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
         theme = 'dark';
     }
   }
+
   setTheme(theme);
 }
 
